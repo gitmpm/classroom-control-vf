23aaa09286e7506ac97bf364cd95af74f65e7136
gitmpm <mpmcspadden@multiservice.com>
2016-10-25 15:19:39 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,7 +53,7 @@ node default {
   exec { 'new motd':
    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
    creates => '/etc/motd',
-   path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin/cowsay',],
+   path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin',],
         }
   file { '/root/deleteme':
   ensure => 'absent',
