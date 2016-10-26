95529071910ab4f30304810a40c41c50b9b2b625
gitmpm <mpmcspadden@multiservice.com>
2016-10-26 10:54:47 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,19 +1,18 @@
-# users/manifests/init.pp
+# skeleton/manifests/init.pp
 class skeleton {
-file { '/etc/skel/':
+file { '/etc/skel':
   ensure => 'directory',
   group  => '0',
-  mode   => '0550',
+  mode   => '0755',
   owner  => '0',
-#  type   => 'directory',
   }
 
-file { '/etc/skel/.bashrc':
+  file { '/etc/skel/.bashrc':
   ensure  => 'file',
   group   => '0',
   mode    => '0644',
   owner   => '0',
-  type    => 'file',
   source  => 'puppet:///modules/skeleton/bashrc',
-  }
+}
+
 }
