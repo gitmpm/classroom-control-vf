31d18aeb94fa8938d6b4b194113678397f19582b
gitmpm <mpmcspadden@multiservice.com>
2016-10-27 09:28:25 -0500

    added variables

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,57 +1,53 @@
 # nginx/manifests/init.pp
 class nginx {
 
-package { 'nginx':
-  ensure => present,
+  $www = '/var/www'
+  $conf_dir = '/etc/nginx/conf.d'
+  $nginx_dir = '/etc/nginx'
+  $nginx_files = 'puppet:///modules/nginx'
+  
+  
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { ${www}: 
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  
+  file { ${www}/index.html:
+    ensure => file,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    source => ${nginx_files}/index.html,
+  }
+  
+  file { ${conf_dir}/default.conf:
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => ${nginx_files}/default.conf,
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  
+  file { ${nginx_dir}/nginx.conf:
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => ${nginx_files}/nginx.conf,
+    require => Package['nginx'],
+    notify  => Service['nginx'],
 }
-
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/nginx/nginx.conf',
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/conf.d/default.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/nginx/default.conf',
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/conf.d':
-  ensure  => directory,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0755',
-  require => Package['nginx'],
-}
-
-file { '/var/www':
-  ensure  => directory,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0755',
-  require => Package['nginx'],
-}
-
-file { '/var/www/index.html':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/nginx/index.html',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
-}
-
+    
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 }
